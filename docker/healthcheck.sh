2b77fcc2d4dcdf45381093bf82bc5a4dad085798
chengsheng <chengsheng@travelsky.com.cn>
2023-01-04 10:37:08 +0800

    添加docker鏡像制作

---
+++ docker/healthcheck.sh
@@ -0,0 +1,11 @@
+## 通过判断接口是否正常来判断应用是否正常
+curl -i --connect-timeout 3 http://127.0.0.1:8048
+_RET=$?
+if [ $_RET != 0 ]; then
+  # 杀死容器主进程，让其重启
+  ps -ef | grep tail | head -1 |awk '{print $2}' | xargs -I {} kill -9 {}
+  exit 1
+else
+  echo '正常'
+  exit 0
+fi
